6df00fe445e917e852ecb2305e3e7f62d7bfa19c
tengda <kidal@vip.qq.com>
2020-08-13 16:22:49 +0800

    语法糖工具类: BeanAccessor

--- jsf-webflux/src/main/java/org/kidal/jsf/webflux/websocket/MessageHandler.java
+++ jsf-webflux/src/main/java/org/kidal/jsf/webflux/websocket/MessageHandler.java
@@ -1,6 +1,7 @@
 package org.kidal.jsf.webflux.websocket;
 
 import org.jetbrains.annotations.NotNull;
+import org.kidal.jsf.core.unify.UnifiedApiContext;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -15,36 +16,50 @@ public class MessageHandler {
   /**
    * 目标
    */
+  @NotNull
   private final Object bean;
 
   /**
    * 方法
    */
+  @NotNull
   private final Method method;
 
   /**
    *
    */
-  public MessageHandler(Object bean, Method method) {
+  private final boolean unified;
+
+  /**
+   *
+   */
+  public MessageHandler(@NotNull Object bean, @NotNull Method method) {
     this.bean = bean;
     this.method = method;
+    this.unified = method.getParameterTypes()[0] == UnifiedApiContext.class;
   }
 
   /**
    * 处理消息
    */
   public Object handle(@NotNull WebSocketMessageHandlingContext context) throws InvocationTargetException, IllegalAccessException {
-    return method.invoke(bean, context);
+    if (unified) {
+      return method.invoke(bean, new UnifiedApiContext(context, context.getParameters()));
+    } else {
+      return method.invoke(bean, context);
+    }
   }
 
   //--------------------------------------------------------------------------
   //
   //--------------------------------------------------------------------------
 
+  @NotNull
   public Object getBean() {
     return bean;
   }
 
+  @NotNull
   public Method getMethod() {
     return method;
   }
