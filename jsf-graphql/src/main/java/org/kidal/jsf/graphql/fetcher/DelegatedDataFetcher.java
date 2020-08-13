e235f7b4bf815d630901ad8c6c776e1d2fa99dcc
tengda <kidal@vip.qq.com>
2020-08-13 22:14:41 +0800

    fix(graphql): 类型转换错误

--- jsf-graphql/src/main/java/org/kidal/jsf/graphql/fetcher/DelegatedDataFetcher.java
+++ jsf-graphql/src/main/java/org/kidal/jsf/graphql/fetcher/DelegatedDataFetcher.java
@@ -1,16 +1,14 @@
 package org.kidal.jsf.graphql.fetcher;
 
-import org.apache.commons.beanutils.BeanUtils;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.kidal.jsf.core.pagination.PageArgs;
 import org.kidal.jsf.core.unify.UnifiedApiContext;
+import org.kidal.jsf.core.utils.JsonUtils;
 import org.kidal.jsf.graphql.annotation.GraphqlParameters;
 import org.kidal.jsf.graphql.query.GraphqlFetchingEnvironment;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
@@ -62,6 +60,7 @@ public class DelegatedDataFetcher extends BaseGraphqlDataFetcher<Object> {
   @Nullable
   @Override
   public Object fetch(@NotNull GraphqlFetchingEnvironment env) throws Exception {
+    // 参数
     Object[] parameters = new Object[method.getParameterCount()];
     for (int i = 0; i < method.getParameterCount(); i++) {
       Class<?> type = method.getParameterTypes()[i];
@@ -74,13 +73,10 @@ public class DelegatedDataFetcher extends BaseGraphqlDataFetcher<Object> {
       } else {
         Annotation[] annotations = method.getParameterAnnotations()[i];
         if (annotations.length > 0 && annotations[0] instanceof GraphqlParameters) {
-          try {
-            Object parameter = type.newInstance();
-            BeanUtils.populate(parameter, env.getEnvironment().getArguments());
-            parameters[i] = parameter;
-          } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            ExceptionUtils.rethrow(e);
-          }
+          // TODO: 优化性能
+          String argumentsJson = JsonUtils.toString(env.getEnvironment().getArguments());
+          Object parameter = JsonUtils.toObject(argumentsJson, type);
+          parameters[i] = parameter;
         }
       }
     }
