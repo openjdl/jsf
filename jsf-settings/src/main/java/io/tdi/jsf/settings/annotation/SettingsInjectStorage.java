fa5eee54d1f5c7d7be38548a19ab2bed04ce7a9a
tengda <kidal@vip.qq.com>
2020-09-11 11:57:46 +0800

    feat(settings): 设置系统

--- jsf-settings/src/main/java/io/tdi/jsf/settings/annotation/SettingsInjectStorage.java
+++ jsf-settings/src/main/java/io/tdi/jsf/settings/annotation/SettingsInjectStorage.java
@@ -16,7 +16,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface InjectSettingsStorage {
+public @interface SettingsInjectStorage {
   /**
    * {@link #id()}
    */
